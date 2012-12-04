672ef6ea8203d1ea079ee3da3eedcaa9f12b1c31
Ketan Soni <ketansony@gmail.com>
2012-12-04 14:20:46 +0530

    Ketan - Removed the javascript test strategy

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,7 @@
 require 'cucumber/rails'
 
 DatabaseCleaner.strategy = nil
-Cucumber::Rails::Database.javascript_strategy = :truncation
+Cucumber::Rails::Database.javascript_strategy = :transaction
 ActionController::Base.allow_rescue = false
 Capybara.default_selector = :css
 Capybara.javascript_driver = :selenium
